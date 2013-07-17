b2b6bb6840b5b99f72d663f1e395fb0fbe937b9e
Sadukow <sadukow@gmail.com>
2013-07-17 11:56:31 +0400

    FVD - FireFox

---
+++ artur.dubovoy@gmail.com/chrome/content/include/script.js
@@ -0,0 +1,126 @@
+FVDSINGLE_Page = {
+	
+	init: function(){
+		try
+		{
+			document.getElementById( "appcontent" ).addEventListener("DOMContentLoaded", function( event ){
+				FVDSINGLE_Page.loadPageListener( event );
+			}, true);
+		}
+		catch( ex ){
+			dump( "Fail init YTButton listener. ("+ex+")\r\n" );
+		}
+		
+	},
+	
+	alert: function(text)
+	{
+		var aConsoleService = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
+		aConsoleService.logStringMessage(text);
+	},
+	
+	loadPageListener: function( e ){
+		
+		try
+		{
+			var win = e.target.defaultView;
+		    if (win.wrappedJSObject)		win = win.wrappedJSObject;				
+		    var loc = new XPCNativeWrapper(win, "location").location;
+			var href = new XPCNativeWrapper(loc, "href").href;
+//			this.alert('---- loadPageListener ----');
+//			this.alert('loc = ' + loc);
+//			this.alert('href =' + href);
+			
+           	var script = this.getUrlContents('chrome://fvd.single/content/include/site_page.js');
+	
+           	this.insertScript(script, href, win);
+
+
+		}
+		catch( ex )
+		{
+			dump( "Fail insert FVD Code to page("+ex+").\r\n" );
+		}
+		
+	},
+
+	insertScript: function( script, href, win ){
+		
+		if( typeof fvd_single != "undefined" )
+		{
+			if( fvd_single.silentMode )
+			{
+				return false;// silent mode activated, no add button
+			}
+		}
+		
+		win = new XPCNativeWrapper( win );
+		var sandbox = new Components.utils.Sandbox(win);
+		
+		sandbox.url = href;
+		sandbox.window = win;
+		sandbox.document = win.document;		
+		 
+        try 
+		{		
+			this.evalInSandbox("(function(){" + script + "})()", href, sandbox);
+		} 
+		catch (ex) 
+		{
+			dump( "FVD_Single: Expception while eval in sandbox - " + href + "("+ex+")\r\n" );
+		}	
+			
+	},
+	
+	evalInSandbox: function (code, codebase, sandbox) {
+        if (Components.utils && Components.utils.Sandbox) 
+		{
+            // DP beta+
+			try 
+			{
+				Components.utils.evalInSandbox(code, sandbox);
+			}		
+			catch (ex)   {			this.alert('**!!!**'+ex);    }
+        } 
+		else if (Components.utils && Components.utils.evalInSandbox) 
+		{
+            // DP alphas
+			try 
+			{
+				Components.utils.evalInSandbox(code, codebase, sandbox);
+			}		
+			catch (ex)   {			this.alert('**!*!*!**'+ex);    }
+        } 
+		else if (Sandbox) 
+		{
+            // 1.0.x
+			try 
+			{
+				evalInSandbox(code, sandbox, codebase);
+			}		
+			catch (ex)   {			this.alert('**!*!!!*!**'+ex);    }
+        } 
+		else 
+		{
+            dump( "SPSingle: Cannot eval in sandbox\r\n" );
+        }
+    },
+	
+	getUrlContents: function (aUrl) {
+        var ioService = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+        var scriptableStream = Components.classes["@mozilla.org/scriptableinputstream;1"].getService(Components.interfaces.nsIScriptableInputStream);
+
+        var channel = ioService.newChannel(aUrl, null, null);
+        var input = channel.open();
+        scriptableStream.init(input);
+        var str = scriptableStream.read(input.available());
+        scriptableStream.close();
+        input.close();
+        return str;
+    },
+	
+
+	
+}
+
+window.addEventListener("load", FVDSINGLE_Page.init, true);
---
+++ artur.dubovoy@gmail.com/defaults/preferences/fvd_single_setup.js
@@ -0,0 +1,86 @@
+pref('fvd_single.supported_sites.check_interval', "2592000000"); // 4 days
+                                                   
+pref('fvd_single.in_new_tab', true); // open pages in new tabs
+
+pref('fvd_single.hotkey.button', 'L'); // default hotkey button
+pref('fvd_single.hotkey.modifier', 'alt'); // default hotkey modifer addon
+
+pref('fvd_single.display.statusbar_button', false); // display statusbar button (only for ff3)
+
+pref('fvd_single.download.custom', false); // false - default folder, true - user folder
+//pref('fvd_single.download.always_in', false); // false - save as, true - save into... 
+
+pref('fvd_single.license.usage.agree', true);
+pref('fvd_single.license.adult.agree', false);
+
+pref("fvd_single.single.dont_display_features_hint", false);
+
+pref("fvd_single.download.always_on_top", true);
+pref("fvd_single.download.close_after_download", false);
+
+pref("fvd_single.default_mode", "compact");
+
+pref("fvd_single.counters.youtube", 0);
+pref("fvd_single.counters.like_message_after", 10);
+pref("fvd_single.counters.like_status", "no_action"); /* like, already_done */
+
+pref("fvd_single.display_youtube_button", true);
+
+pref("fvd_single.dont_display_fvdsd_hint", false);
+pref("fvd_single.fvdsd_hint_last_display_time", "0");
+
+pref("fvd_single.fvdsd_last_used_version", "");
+pref("fvd_single.is_first_run", true);
+
+// Show Formats
+pref("fvd_single.enable_type_mp4", true);
+pref("fvd_single.enable_type_3gp", true);
+pref("fvd_single.enable_type_flv", true);
+pref("fvd_single.enable_type_mov", true);
+pref("fvd_single.enable_type_avi", true);
+pref("fvd_single.enable_type_wmv", true);
+pref("fvd_single.enable_type_asf", true);
+pref("fvd_single.enable_type_swf", true);
+pref("fvd_single.enable_type_webm", true);
+pref("fvd_single.enable_type_mp3", true);
+pref("fvd_single.enable_type_mid", true);
+pref("fvd_single.enable_type_wav", true);
+pref("fvd_single.enable_type_aif", true);
+
+// Default Formats
+pref("fvd_single.default_type_mp4", true);
+pref("fvd_single.default_type_3gp", true);
+pref("fvd_single.default_type_flv", true);
+pref("fvd_single.default_type_mov", true);
+pref("fvd_single.default_type_avi", true);
+pref("fvd_single.default_type_wmv", true);
+pref("fvd_single.default_type_asf", true);
+pref("fvd_single.default_type_swf", true);
+pref("fvd_single.default_type_webm", true);
+pref("fvd_single.default_type_mp3", true);
+pref("fvd_single.default_type_mid", true);
+pref("fvd_single.default_type_wav", true);
+pref("fvd_single.default_type_aif", true);
+
+// Show Formats YouTube
+pref("fvd_single.enable_yt_type_mp4", true);
+pref("fvd_single.enable_yt_type_3gp", true);
+pref("fvd_single.enable_yt_type_flv", true);
+pref("fvd_single.enable_yt_type_mov", true);
+pref("fvd_single.enable_yt_type_avi", true);
+pref("fvd_single.enable_yt_type_wmv", true);
+pref("fvd_single.enable_yt_type_asf", true);
+pref("fvd_single.enable_yt_type_swf", true);
+pref("fvd_single.enable_yt_type_webm", true);
+
+// Default Formats YouTube
+pref("fvd_single.default_yt_type_mp4", true);
+pref("fvd_single.default_yt_type_3gp", true);
+pref("fvd_single.default_yt_type_flv", true);
+pref("fvd_single.default_yt_type_mov", true);
+pref("fvd_single.default_yt_type_avi", true);
+pref("fvd_single.default_yt_type_wmv", true);
+pref("fvd_single.default_yt_type_asf", true);
+pref("fvd_single.default_yt_type_swf", true);
+pref("fvd_single.default_yt_type_webm", true);
+
